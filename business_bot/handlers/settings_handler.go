0b7587f2fcd35f4032fc2e764da20c4aa958d8e5
sudora1n <me@sudora1n.dev>
2025-06-29 11:55:42 +0300

    final fixes and tested, all good

--- business_bot/handlers/settings_handler.go
+++ business_bot/handlers/settings_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"ssuspy-bot/callbacks"
 	"ssuspy-bot/consts"
+	"ssuspy-bot/repository"
 	"ssuspy-bot/types"
 
 	"github.com/mymmrac/telego"
@@ -14,19 +15,26 @@ import (
 func (h *Handler) HandleSettings(c *th.Context, update telego.Update) error {
 	query := update.CallbackQuery
 	loc := c.Value("loc").(*i18n.Localizer)
-	internalUser := c.Value("internalUser").(*types.InternalUser)
+	iUser := c.Value("iUser").(*repository.IUser)
 
 	needUpdate := true
 	data, err := callbacks.NewHandleSettingsDataFromString(query.Data)
 	if err != nil {
 		if err == callbacks.NoSettingsPartsError {
 			needUpdate = false
+			data = &types.HandleSettingsData{
+				ShowMyDeleted:      iUser.User.Settings.Deleted.ShowMyDeleted,
+				ShowPartnerDeleted: iUser.User.Settings.Deleted.ShowPartnerDeleted,
+				ShowMyEdits:        iUser.User.Settings.Edited.ShowMyEdits,
+				ShowPartnerEdits:   iUser.User.Settings.Edited.ShowPartnerEdits,
+			}
+		} else {
+			return err
 		}
-		return err
 	}
 
 	if needUpdate {
-		err = h.service.UpdateUserSettings(c, internalUser.ID, data)
+		err = h.service.UpdateUserSettings(c, iUser.User.ID, data)
 		if err != nil {
 			return err
 		}
@@ -70,7 +78,7 @@ func (h *Handler) HandleSettings(c *th.Context, update telego.Update) error {
 		loc.MustLocalize(&i18n.LocalizeConfig{
 			MessageID: "settings.partnerDel",
 			TemplateData: map[string]bool{
-				"Status": data.ShowMyDeleted,
+				"Status": data.ShowPartnerDeleted,
 			},
 		}),
 	).WithCallbackData(partnerDelData.ToString())))
@@ -81,7 +89,7 @@ func (h *Handler) HandleSettings(c *th.Context, update telego.Update) error {
 		loc.MustLocalize(&i18n.LocalizeConfig{
 			MessageID: "settings.myEdit",
 			TemplateData: map[string]bool{
-				"Status": data.ShowMyDeleted,
+				"Status": data.ShowMyEdits,
 			},
 		}),
 	).WithCallbackData(myEditData.ToString())))
@@ -92,7 +100,7 @@ func (h *Handler) HandleSettings(c *th.Context, update telego.Update) error {
 		loc.MustLocalize(&i18n.LocalizeConfig{
 			MessageID: "settings.partnerEdit",
 			TemplateData: map[string]bool{
-				"Status": data.ShowMyDeleted,
+				"Status": data.ShowPartnerEdits,
 			},
 		}),
 	).WithCallbackData(partnerEditData.ToString())))
@@ -104,7 +112,7 @@ func (h *Handler) HandleSettings(c *th.Context, update telego.Update) error {
 	).WithCallbackData(consts.CALLBACK_PREFIX_BACK_TO_START)))
 
 	_, err = c.Bot().EditMessageText(c, tu.EditMessageText(
-		tu.ID(internalUser.ID),
+		tu.ID(iUser.User.ID),
 		query.Message.GetMessageID(),
 		messageText,
 	).WithParseMode(telego.ModeHTML).WithReplyMarkup(tu.InlineKeyboard(rows...)))
